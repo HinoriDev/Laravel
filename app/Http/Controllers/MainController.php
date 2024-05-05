0deb67ff55cac4d4f6130c540ca6e3a439d6b9d3
João Ribeiro <jribeiro@gmail.com>
2024-05-05 15:06:17 +0100

    004

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,38 +9,6 @@ class MainController extends Controller
 {
     public function showView(): View
     {
-        // ---------------------------------------
-        // método 1
-        /*
-        $data = [
-            'name' => "João Ribeiro",
-            'phone' => "123456789"
-        ];
-        return view('admin.newPage3', $data);
-        */
-
-        // ---------------------------------------
-        // método 2
-        /*
-        return view('admin.newPage3', [
-            'name' => "João Ribeiro",
-            'phone' => "123456789"
-        ]);
-        */
-
-        // ---------------------------------------
-        // método 3
-        /*
-        return view('admin.newPage3')
-                ->with('name', "João Ribeiro")
-                ->with('phone', '123456789');
-        */
-
-        // ---------------------------------------
-        // método 4
-        $name = "João";
-        $phone = "123000000";
-
-        return view('admin.newPage3', compact('name', 'phone'));
+        return view('home', ['name' => 'John Doe']);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts/main_layout')
 @section('content')
 
-<p class="display-1 text-center">Hello World</p>
-
+{{-- comentário de blade --}}
+<p><?= $name ?></p>
+<p>{{ $name }}</p>
 @endsection
