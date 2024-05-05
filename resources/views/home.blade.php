e069bd0d644926ff5a2e2b41bc161cc3d411d7ad
João Ribeiro <jribeiro@gmail.com>
2024-05-05 15:14:55 +0100

    005

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,36 @@
 @extends('layouts/main_layout')
 @section('content')
 
-{{-- comentário de blade --}}
-<p><?= $name ?></p>
-<p>{{ $name }}</p>
+{{-- instrução IF simples --}}
+@if($value == 100)
+    <h1>Primeira parte do código</h1>
+@endif
+
+{{-- instrução IF ELSE --}}
+@if($value != 100)
+    <h1>Primeira parte do código</h1>
+@else
+    <h1>Segunda parte do código</h1>
+@endif
+
+{{-- instrução IF ELSEIF ELSE ENDIF --}}
+@if($value < 10)
+    <p>Primeiro</p>
+    <div>
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+@elseif($value < 20)
+    <p>Segundo</p>
+@elseif($value > 50)
+    <p>Terceiro</p>
+@elseif($value == 100)
+    <p>Quarto</p>
+@else
+    <p>Outro caso</p>
+@endif
+
 @endsection
