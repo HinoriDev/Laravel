893ec28e6ec89e8cf36a1f7160c02e168a3df5c6
hinori <hinori@gmail.com>
2026-05-21 17:44:57 -0300

    feat: organizando todos os projetos na raiz

--- laravel 11/_GLOBAL_FILES_AND_FOLDERS/recursos_rh_mangnt/layouts/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- resources -->
-<script src="assets/datatables/jquery.min.js"></script>
-    <script src="assets/bootstrap/bootstrap.bundle.min.js"></script>
-    <script src="assets/datatables/datatables.min.js"></script>
-
-</body>
-
-</html>
