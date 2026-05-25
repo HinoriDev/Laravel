1071105532277336a71b27f55b8cf2aa72583dbb
hinori <hinori@gmail.com>
2026-05-25 14:37:10 -0300

    blades parte 4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,28 @@
 @extends('layouts/main_layout')
 @section('content')
 
-{{-- comentário de blade --}}
-<p><?= $name ?></p>
-<p>{{ $name }}</p>
+{{-- instrução IF simples --}}
+@if($value == 100)
+<h1>Primeira parte do código</h1>
+@endif
+
+{{--Instrução IF ELSE --}}
+@if($value != 100)
+<h1>Primeira parte do codigo</h1>
+@else
+<h1>Segunda parte do codigo</h1>
+@endif
+
+{{--Instrução IF ELSEIF ELSE ENDIF --}}
+@if($value < 10)
+<p>Primeiro</p>
+@elseif($value < 20)
+<p>Segundo</p>
+@elseif($value > 50)
+<p>Terceiro</p>
+@elseif($value == 100)
+<p>Quarto</p>
+@else
+<p>Outro caso</p>
+@endif
 @endsection
