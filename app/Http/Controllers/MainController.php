ba3f24e565c811086bcaf3d9fc28441aaeaf3978
hinori <hinori@gmail.com>
2026-05-25 14:20:10 -0300

    blades parte 2

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,6 +9,38 @@ class MainController extends Controller
 {
     public function showView(): View
     {
-        return view('home');
+        //método 1
+        /*
+        $data = [
+            'name' => 'Hinori',
+            'phone' => "123456789"
+        ];
+        return view('admin.newPage3', $data);
+        */
+
+        //---------------------------------------------------------
+        //método 2
+        /*
+        return view('admin.newPage3', [
+            'name' => 'Hinori',
+            'phone' => "123456789"
+        ]);
+        */
+
+        //--------------------------------------------------------
+        //método 3
+        /*
+        return view('admin.newPage3')
+        ->with('name', "Hinori")
+        ->with('phone', "123456789");
+        */
+
+        //---------------------------------------------------------
+        //método 4
+        $name = "Hinori";
+        $phone = "123456789";
+
+        return view('admin.newPage3', compact('name', 'phone'));
+        
     }
 }
---
+++ resources/views/admin/newPage3.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts/main_layout')
+@section('content')
+
+<h1>{{$name}} {{$phone}}</h1>
+
+@endsection
