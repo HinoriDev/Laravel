cad7689bba7cc986298858023ac33231d0fe3740
hinori <hinori@gmail.com>
2026-05-25 14:49:58 -0300

    blades parte 6

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,22 @@
 @extends('layouts/main_layout')
 @section('content')
 
-{{-- switch --}}
-@switch($value)
-    @case(100)
-        <h1>Valor 100</h1>
-        @break
-    @case(200)
-        <h1>Valor 200</h1>
-        @break
-    @case(300)
-        <h1>Valor 300</h1>
-        @break
-    @default
-        <h1>Outro!</h1>
-@endswitch
+{{-- empty --}}
+@empty($value)
+<p>Não existe</p>
+@else
+<p>Existe</p>
+@endempty
+{{-- isset --}}
+@isset($value)
+<p>Existe a variável</p>
+@else   
+<p>Não existe a variável</p>
+@endisset
+{{-- unless --}}
+@unless($value != 100)
+<p>Ok!!!!</p>
+
+@endunless
 
 @endsection
